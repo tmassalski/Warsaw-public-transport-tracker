303d2c109726b897788685929aaf579c535d983b
Tomasz Massalski <tomasz.massalski@gmail.com>
2020-04-04 10:27:07 +0200

    Initial commit

---
+++ src/main/java/com/tmassalski/warsawpublictransportlocator/WarsawPublicTransportLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.tmassalski.warsawpublictransportlocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsawPublicTransportLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarsawPublicTransportLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmassalski/warsawpublictransportlocator/WarsawPublicTransportLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmassalski.warsawpublictransportlocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarsawPublicTransportLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
